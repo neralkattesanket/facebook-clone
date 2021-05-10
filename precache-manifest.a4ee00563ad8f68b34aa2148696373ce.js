75d6d34698bc219815eb5a2cb3029182cbe7f669
neralkattesanket <neralkattesanket@gmail.com>
2021-05-11 02:07:06 +0530

    Updates

---
+++ precache-manifest.a4ee00563ad8f68b34aa2148696373ce.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "82ccc662d678ece84d39",
+    "url": "/facebook-clone/static/css/main.16859569.chunk.css"
+  },
+  {
+    "revision": "82ccc662d678ece84d39",
+    "url": "/facebook-clone/static/js/main.3214ce15.chunk.js"
+  },
+  {
+    "revision": "2f839d5a29094a735cdb",
+    "url": "/facebook-clone/static/js/runtime~main.b8910374.js"
+  },
+  {
+    "revision": "0444e081d08ec0563474",
+    "url": "/facebook-clone/static/js/2.bca92e6b.chunk.js"
+  },
+  {
+    "revision": "0ba380033aecfd616a40",
+    "url": "/facebook-clone/static/js/3.40d1f8f4.chunk.js"
+  },
+  {
+    "revision": "94e8550c1a27aeb28e2bf7c1df87c44b",
+    "url": "/facebook-clone/index.html"
+  }
+];
